bf805a50512c847d930ab6e6784129a3b0c10793
AbdelMonaam Aouini <moneem.aouni@wssel.com>
2022-01-13 14:30:29 +0100

    fix - fix UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import {
+  Container,
+  FormControl,
+  Grid,
+  InputLabel,
+  makeStyles,
+  MenuItem,
+  Select,
+} from "@material-ui/core";
+import { Maps } from "./components";
+import CountriesList from "./data/countriesList.json";
+import "./App.css";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    marginTop: 10,
+    marginBottom: 20,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    if (CountriesList) {
+      setCountries(CountriesList);
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Grid xs={3} justify="center">
+          <FormControl fullWidth className={classes.formControl}>
+            <InputLabel htmlFor="countries-simple">Countries</InputLabel>
+            <Select
+              label="countries"
+              onChange={handleChange}
+              inputProps={{
+                name: "countries",
+                id: "countries-simple",
+              }}
+            >
+              {countries.map((country) => {
+                return (
+                  <MenuItem value={country.id} key={country.id}>
+                    {country.name}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid>
+          <Maps />
+        </Grid>
+      </Container>
+    </>
   );
 }
 
